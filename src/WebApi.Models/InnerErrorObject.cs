74e3cdb63975748d88749db4f87260de9ca18a24
doricat <doricat@outlook.com>
2020-11-27 17:15:53 +0800

    删除不必要的属性

--- src/WebApi.Models/InnerErrorObject.cs
+++ src/WebApi.Models/InnerErrorObject.cs
@@ -1,13 +1,9 @@
-﻿using System.Collections.Generic;
-
-namespace WebApi.Models
+﻿namespace WebApi.Models
 {
     public class InnerErrorObject
     {
         public string Code { get; set; }
 
         public InnerErrorObject InnerError { get; set; }
-
-        public IDictionary<string, object> Props { get; set; }
     }
 }
